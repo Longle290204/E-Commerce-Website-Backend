ed7c6b812992cb98b96e55beca6f35498be40481
LongLe04 <longvis2004@gmail.com>
2024-11-24 02:17:14 +0700

    feat: add product and category

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -3,9 +3,29 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product~entity';
-
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { CategoryModule } from 'src/category/category.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    CategoryModule,
+    // Cấu hình xử lý upload files
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${file.fieldname}-${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+    //
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
---
+++ src/products/product~category.enum.ts
@@ -0,0 +1,4 @@
+export enum ProductCategory {
+  NAM = 'NAM',
+  Nu = 'NU',
+}
